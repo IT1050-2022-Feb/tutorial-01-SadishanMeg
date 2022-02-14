bce6ef689412bcb54fc3a639ad020e49dcd26d3e
Sadishan <90977747+SadishanMeg@users.noreply.github.com>
2022-02-14 17:24:26 +0000

    What did you change?

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+  int no1 , no2;
+	float average;
+	
+	printf("Enter Number 1 and Number 2: ");
+	scanf("%d %d" , &no1 , &no2);
+	
+	average = (float) (no1 + no2) / 2;
+	
+	printf("\n");
+	printf("The Average is : %.2f\n" , average);
+	
   return 0;
 }
 
